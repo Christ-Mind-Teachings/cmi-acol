f665838443a7024570099a0787a2f326bad13fde
Rick Mercer <rmercer33@gmail.com>
2023-03-29 21:55:48 -1000

    added support for saved searches

--- src/js/transcript.js
+++ src/js/transcript.js
@@ -1,5 +1,6 @@
 /* eslint no-console: off */
-import {storeInit} from "www/modules/_util/store";
+import {SourceStore, storeInit} from "www/modules/_util/store";
+import search from "www/modules/_search/search";
 
 //common modules
 import {showParagraph} from "www/modules/_util/url";
@@ -10,7 +11,7 @@ import {initTranscriptPage} from "www/modules/_page/startup";
 //teaching specific modules
 import {loadConfig} from "./modules/_config/config";
 import {bookmarkStart} from "./modules/_bookmark/start";
-import search from "./modules/_search/search";
+import { searchInit } from "./modules/_search/search";
 import toc, {getBookId} from "./modules/_contents/toc";
 import audio from "./modules/_audio/audio";
 import about from "./modules/_about/about";
@@ -20,7 +21,9 @@ import {setLanguage} from "www/modules/_language/lang";
 import constants from "./constants";
 
 $(document).ready(() => {
+  const store = new SourceStore(constants);
   storeInit(constants);
+
   setLanguage(constants);
   initTranscriptPage("pnDisplay");
   auth.initialize();
@@ -31,7 +34,7 @@ $(document).ready(() => {
   acol.initialize();
 
   loadConfig(getBookId()).then((result) => {
-    search.initialize();
+    search.initialize(searchInit(store));
     toc.initialize("transcript");
     audio.initialize();
     showParagraph();
