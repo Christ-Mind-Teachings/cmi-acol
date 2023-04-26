a717d92ec28cb9d0c1dc86ae737bf304cec2721e
Rick Mercer <rmercer33@gmail.com>
2023-04-25 21:09:06 -1000

    moved local source and bookmark init to common

--- src/js/transcript.js
+++ src/js/transcript.js
@@ -3,17 +3,12 @@
 import {SourceStore, storeInit} from "common/modules/_util/store";
 import search from "common/modules/_search/search";
 import {showParagraph} from "common/modules/_util/url";
-import auth from "common/modules/_user/netlify";
-import fb from "common/modules/_util/facebook";
-import {initTranscriptPage} from "common/modules/_page/startup";
 import audio from "common/modules/_audio/audio";
+import {initTranscriptPage} from "common/modules/_page/startup";
 
 //teaching specific modules
 import {setEnv, loadConfig} from "./modules/_config/config";
-import {bookmarkStart} from "./modules/_bookmark/start";
-import { searchInit } from "./modules/_search/search";
 import toc, {getBookId} from "./modules/_contents/toc";
-import about from "./modules/_about/about";
 import acol from "./modules/_acol/acol";
 
 import constants from "./constants";
@@ -21,11 +16,6 @@ import constants from "./constants";
 $(document).ready(() => {
   const store = new SourceStore(constants);
   storeInit(constants);
-  auth.initialize();
-
-  initTranscriptPage("pnDisplay");
-  fb.initialize();
-  about.initialize();
 
   setEnv(store);
 
@@ -33,11 +23,11 @@ $(document).ready(() => {
   acol.initialize();
 
   loadConfig(getBookId()).then((result) => {
-    search.initialize(searchInit(store));
+    initTranscriptPage(store);
+    search.initialize(store);
     toc.initialize("transcript");
     audio.initialize(store);
     showParagraph();
-    bookmarkStart("transcript", store);
   }).catch((error) => {
     console.error(error);
   });
